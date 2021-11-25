a567910f8312fe4240d1fa83af9c8616a6b52c05
Laura Quintero <lcquintero@personalsoft.com>
2021-11-25 12:47:47 -0500

    UpdatePipeline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,32 +1,37 @@
-pipeline {
-	agent any
+pipeline { 
+agent any 
 	stages {
-		stage ('build sin test') { 
-		steps { 
-			nodejs (nodeJSInstallationName: 'nodejs'){
-				sh 'npm install'
+		stage('build sin test') { 
+			steps {
+				nodejs(nodeJSInstallationName: 'nodejs') { sh 'npm install'
 				sh 'npm rebuild'
 				sh 'npm run build --skip-test --if-present'
+// stash name: "ws", includes: "**"
 			}
 		}
 	}
-	
-	stage ('unitTest') {
+
+
+	stage('unitTest') { 
 		steps {
-			nodejs (nodeJSInstallationName: 'nodejs'){
-				sh 'npm run test: coverage && cp coverage/lcov.info icov.info || echo "Code coverage failed"'
-				archiveArtifacts (artifacts: 'coverage/**', onlyIfSuccessful:true)
-			}
-		}
-	}	
-	
-	stage ('deploy') {
-		steps {
-			nodejs (nodeJSInstallationName: 'nodejs')
-				withAWS (credentials: 'aws-credentials'){
-					sh 'serverless deploy'
-				}
+			//unstash "ws"
+			nodejs(nodeJSInstallationName: 'nodejs') {
+				sh 'npm run test:coverage && cp coverage/lcov.info lcov.info || echo "Code coverage failed"'
+				archiveArtifacts(artifacts: 'coverage/**', onlyIfSuccessful: true)
 			}
 		}
 	}
+
+
+	stage('deploy') { 
+		steps {
+			nodejs(nodeJSInstallationName: 'nodejs') {
+				withAWS(credentials: 'aws-credentials') { sh 'serverless deploy'
+			}
+		}
+	}
+}
+
+
+}
 }
